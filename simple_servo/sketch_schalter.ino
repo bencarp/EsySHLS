4b4b6d12ac190e03d6854740cb9b68301b5ae8e6
EmileDev <emile2002@web.de>
2016-10-15 14:53:35 +0200

    Sketch für D1 MINI

---
+++ simple_servo/sketch_schalter.ino
@@ -0,0 +1,63 @@
+
+#include <Servo.h>
+
+Servo servosg90;
+int ledPingruen = 15;
+int ledPinrot = 13;
+
+//button
+int inPin = 12;   // pushbutton connected to digital pin 7
+int val = 0;     // variable to store the read value
+//button end
+
+void setup()
+
+{
+pinMode(ledPingruen, OUTPUT);
+pinMode(ledPinrot, OUTPUT);
+
+servosg90.attach(14);
+
+
+//button
+ pinMode(inPin, INPUT_PULLUP);      // sets the digital pin 7 as input
+
+  Serial.begin(9600);
+//button end
+
+
+}
+
+ 
+void loop()
+
+{
+  //button
+  val = digitalRead(inPin);   // read the input pin
+  //digitalWrite(ledPin, val);   
+
+  Serial.println(val);             // debug value
+  delay(100);
+  //button end
+
+
+  
+  digitalWrite(ledPingruen, 1);
+
+
+  if(val==0){
+
+  digitalWrite(ledPingruen, 0);
+  digitalWrite(ledPinrot, 1);
+  servosg90.write(0);
+
+  delay(500);
+
+  servosg90.write(120);
+
+  
+  delay(500);
+  digitalWrite(ledPinrot, 0);
+  digitalWrite(ledPingruen, 1);
+
+}}
